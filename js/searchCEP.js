e8c913961a09990539f8a37fde473ad352c75eff
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-11-19 00:11:40 -0300

    criação do arquivo searchCEP.js. atualização de apontamento do arquivo em cadastro1.html.

---
+++ js/searchCEP.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const writeForm = (addressData) => {
+    document.getElementById('lougradouro').value = addressData.logradouro
+    document.getElementById('bairro').value = addressData.bairro
+    document.getElementById('cidade').value = addressData.localidade
+    document.getElementById('estado').value = addressData.uf
+}
+
+const clearForm = () => {
+    document.getElementById('lougradouro').value = ""
+    document.getElementById('bairro').value = ""
+    document.getElementById('cidade').value = ""
+    document.getElementById('estado').value = ""
+}
+
+const validCep = (cep) => /^[0-9]+$/.test(cep) && cep.length == 8; 
+
+const searchCEP = async() =>{
+    clearForm();
+    const cep = document.getElementById('CEP').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    if (validCep(cep)){
+        const jsonData = await fetch(url)
+        const addressData = await jsonData.json();        
+        if (addressData.hasOwnProperty('ibge')){
+            writeForm(addressData);
+        }else{
+            alert ('CEP não encontrado')
+        }    
+    }else{
+        alert ('CEP inválido')
+    }
+};
+
+document.getElementById('CEP')
+    .addEventListener('focusout',searchCEP);
