be1c44a5ce2da93d0cc7f356c4e0e39053685582
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-11-23 15:58:30 -0300

    atuzalização de parametrização do imput do cep. url de cadastro apontando para o cadastroCPF.html adicionada

--- js/searchCEP.js
+++ js/searchCEP.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var cepNum = 'CEP'
+
 const writeForm = (addressData) => {
     document.getElementById('lougradouro').value = addressData.logradouro
     document.getElementById('bairro').value = addressData.bairro
@@ -18,7 +20,7 @@ const validCep = (cep) => /^[0-9]+$/.test(cep) && cep.length == 8;
 
 const searchCEP = async() =>{
     clearForm();
-    const cep = document.getElementById('CEP').value;
+    const cep = document.getElementById('CEP').value.replace(/\D/g, "");
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     if (validCep(cep)){
         const jsonData = await fetch(url)
@@ -33,4 +35,17 @@ const searchCEP = async() =>{
     }
 };
 
-document.getElementById('CEP').addEventListener('focusout',searchCEP);
+function cepMask() {
+
+    document.getElementById(cepNum).value = document.getElementById(cepNum).value.replace(/[&\/\\#,+()$~%'":*?<>{}a-zA-Z]/g,'');
+
+    if(document.getElementById(cepNum).value.length == 2){
+        document.getElementById(cepNum).value += "."      
+    }else if(document.getElementById(cepNum).value.length == 6){        
+        document.getElementById(cepNum).value += "-"
+    };
+};  
+
+document.getElementById(cepNum).addEventListener('focusout',searchCEP);
+document.getElementById(cepNum).addEventListener('keydown',cepMask);
+
