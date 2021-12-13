3e4e2a61ac1733fed678e37c4edd735d047b8288
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-12-13 13:30:00 -0300

    atualizacao do css em cadastro-perfil, atualizacao do js em loginvalidation

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function loginItensValidation(){
+function loginItensValidation(form){
 
     let loginDefault = "test@email.com"
     let passwDefault = "123123"
@@ -11,25 +11,32 @@ function loginItensValidation(){
     let message = document.getElementById('message');
     let msg = ""
 
-    if (login === loginDefault && passw === passwDefault){
-        document.getElementById('message').style.display = 'none'
-        window.location.href = "historico.html";
+    if (localStorage.getItem('typebottom') == 0){
+        if (login === loginDefault && passw === passwDefault){
+            document.getElementById('message').style.display = 'none'
+            form.action = "historico.html";
+        }else{
+            switch(login){
+                case loginDefault:
+                    msg = 'SENHA incorreta!'
+                    break;
+                default:
+                    msg = 'Dados de LOGIN não cadastrado ou inválidos!'
+                    break;
+            }
+            message.innerHTML = msg;
+            message.className = 'message';
+            document.getElementById('senha').value = ""
+            event.preventDefault();
+        };
     }else{
-        switch(login){
-            case loginDefault:
-                msg = 'SENHA incorreta!'
-                break;
-            default:
-                msg = 'Dados de LOGIN não cadastrado ou inválidos!'
-                break;
-        }
-        message.innerHTML = msg;
-        message.className = 'message';
-        document.getElementById('senha').value = ""
-        event.preventDefault();
-    };
+        form.action = "cadastro-perfil.html";
+    }
 };
 
 function clearMsg(){
     document.getElementById('message').style.display = 'none';
+};
+function typebottom(param){
+    localStorage.setItem('typebottom', param)
 };
