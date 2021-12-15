0149fdb01f2266d91f803c2cd4dcd4979bd61175
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-12-14 21:42:35 -0300

    atualizacao de telas de cadastro

--- js/registration.js
+++ js/registration.js
@@ -1,15 +1,21 @@
 "use strict";
 
 function typeRegistration(){
-
     let userTypeValue = localStorage.getItem('value');
-
     if (userTypeValue == 0){
         userTypeValue = "Cidadao";
     } else {
         userTypeValue = "Ouvidor";
     };
-
     var legendElement = document.getElementById('registrationLegend');
     legendElement.innerHTML = "Cadastro do "+userTypeValue;
 }
+
+function pageReturn(){{
+    let userTypeValue = localStorage.getItem('value');
+    if (userTypeValue == 0){
+        location.href='cadastro-cidadao-2.html'
+    } else {
+        location.href='cadastro-ouvidor-2.html'
+    };
+}}
